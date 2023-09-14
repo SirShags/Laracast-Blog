5c74d296cc3326a0cdecf20605de827567300cd3
Trey <fredburr6@gmail.com>
2023-09-14 12:13:28 -0700

    updated multiple documents, using YarmFrontMatter

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -1,10 +1,27 @@
 <?php
 
 namespace App\Models;
-
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Support\Facades\File;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 class Post {
+	public $title;
+	public $excerpt;
+	public $date;
+	public $body;
+	public $slug;
+
+	public function __construct($title, $excerpt, $date, $body, $slug)
+	{
+		$this->title = $title;
+		$this->excerpt = $excerpt;
+		$this->date = $date;
+		$this->body = $body;
+		$this->slug = $slug;
+	}
+
+	
 	public static function find($slug) {
 		if(! file_exists($path = resource_path("posts/{$slug}.html"))) {
 	        throw new ModelNotFoundException();
@@ -14,5 +31,21 @@ class Post {
 	    //     return $post = file_get_contents($path);
 	    // });
 	    return cache()->remember("posts.{slug}", 1200, fn() => file_get_contents($path));
+	}
+
+	public static function all() {
+		return collect(File::files(resource_path('posts')))
+        ->map(function ($file) {
+            return YamlFrontMatter::parseFile($file);
+        })
+        ->map(function ($document) {
+            return $posts[] = new Post(
+                $document->title,
+                $document->excerpt,
+                $document->date,
+                $document->body(),
+                $document->slug
+            );
+        });
 	}
 }
